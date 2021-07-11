528432f6f75f52db7d0b761fd4ca3c83530974ba
Robson Vieira <robson.ppmg@gmail.com>
2021-07-11 18:37:50 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
@@ -1,12 +1,30 @@
 package com.ms.hrpayroll.services;
 
 import com.ms.hrpayroll.entities.Payment;
+import com.ms.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Franchesco", 13894.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+ workerId);
+
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class,uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO tb_worker (name, daily_Income) VALUES ('Bobi', 200.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Jubileu', 43434.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Franchesco', 1232141.0);
