66270d47e73f508daa21d75bc7e781d8941d0b5a
Robson Vieira <robson.ppmg@gmail.com>
2021-07-11 17:03:58 -0300

    Project hr-payroll created

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Bobi', 200.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Jubileu', 43434.0);
